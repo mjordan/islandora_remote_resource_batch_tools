b1d2415275b84f23b481da6c042f48cffa3e73da
Mark Jordan <mjordan@sfu.ca>
2017-11-05 15:52:22 -0800

    Second commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# BagIt Indexer
+# Islandora Remote Resource CSV Conversion Tool
 
+Utility script to convert a CSV file into files suitable for using as input to the Islandora Remote Resource Batch module.
 
 ## System requirements and installation
 
@@ -11,12 +12,42 @@ To install and run this proof of concept indexer, you will need:
 To install the Bagit Indexer:
 
 * Clone the Git repo
-* `cd bagit_indexer`
+* `cd islandora_remote_resource_batch_csv_tool`
 * `php composer.phar install` (or equivalent on your system, e.g., `./composer install`)
 
 ## Running the tool
 
-`php convert_csv.php --csv sample_data/test.csv --template templates/DC.twig`
+`php convert_csv.php --csv sample_data/test.csv --output_dir /tmp/output
+
+```
+--help
+     Show the help page for this command.
+
+--csv <argument>
+     Required. Path to the input CSV file.
+
+--output_dir <argument>
+     Required. Path to the output directory.
+
+--template <argument>
+     Required. Path to the Twig template file.
+
+--metadata_prefix <argument>
+     Optional. Path to the output directory. Defaults to "dc_".
+```
+
+## The CSV file
+
+Reserved column headings: `ID` and `OBJ`.
+
+Headings for columns that contain values to be inserted into the metadata template should have a "metadata prefix". This defaults to `dc_`.
+
+Headings for columns that contain filenames that are to be copied into the output directory for loading as datastreams should contains datastream IDs, e.g., `TN`.
+
+## The metadata template
+
+Twig.
+
 
 ## License
 
@@ -26,6 +57,4 @@ To the extent possible under law, Mark Jordan has waived all copyright and relat
 
 ## Contributing
 
-Since this is proof-of-concept code, I don't intend to add a lot more features. However, this proof of concept could be used as the basis for a production application. Fork and enjoy!
-
-That said, if you have any questions or suggestions, feel free to open an issue.
+If you have any questions or suggestions, feel free to open an issue. Also, please open an issue before creating a pull request.
--- convert_csv.php
+++ convert_csv.php
@@ -2,6 +2,10 @@
 
 /**
  * @file
+ * Utility script to convert a CSV file into files suitable for using as input
+ * to the Islandora Remote Resource Batch module.
+ *
+ * For help information, run php convert_csv.php --help
  */
 
 use League\Csv\Reader;
@@ -14,11 +18,14 @@ $cmd->option('csv')
     ->describedAs('Path to the input CSV file.')
     ->file();
 $cmd->option('template')
-    ->require(true)
-    ->describedAs('Path to the Twig template file.');
+    ->default('templates/DC.twig')
+    ->describedAs('Optional. Path to the Twig template file. Defaults to "templates/DC.twig".');
 $cmd->option('output_dir')
     ->require(true)
     ->describedAs('Path to the output directory.');
+$cmd->option('metadata_prefix')
+    ->default('dc_')
+    ->describedAs('Optional. Path to the output directory. Defaults to "dc_".');
 
 $input_csv = Reader::createFromPath($cmd['csv']);
 $records = $input_csv->fetchAssoc();
@@ -31,23 +38,26 @@ foreach ($records as $record) {
     print "Generating files for CSV record " . $record['ID'] . "\n";
 
     foreach ($record as $key => $value) {
+        // First, create the .txt file that contains the URL to the remote resource.
         if ($key == 'OBJ') {
             $dest_path = $cmd['output_dir'] . DIRECTORY_SEPARATOR . $record['ID'] . '.txt';
             file_put_contents($dest_path, $record['OBJ']);
         }
 
-        if ($key != 'ID' || $key!= 'OBJ') {
-            if (!preg_match('/^dc_/', $key)) {
-                $src_path = dirname($cmd['csv']) . DIRECTORY_SEPARATOR . $value;
-                $dest_ext = pathinfo($value, PATHINFO_EXTENSION);
-                $dest_path = $cmd['output_dir'] . DIRECTORY_SEPARATOR . $record['ID'] . '.' . $key . '.' . $dest_ext;
-                if (file_exists($src_path)) {
-                    copy($src_path, $dest_path);
-                    // print "Dest path is $dest_path\n";
-                }
-                else {
-                    print "Source file $value does not exist\n";
-                }
+        // Copy files to the output directory.
+        $metadata_prefix_pattern = '/^' . $cmd['metadata_prefix'] . '/';
+        if ($key == 'ID' || $key == 'OBJ') {
+            continue;
+        }
+        if (!preg_match($metadata_prefix_pattern, $key)) {
+            $src_path = dirname($cmd['csv']) . DIRECTORY_SEPARATOR . $value;
+            $dest_ext = pathinfo($value, PATHINFO_EXTENSION);
+            $dest_path = $cmd['output_dir'] . DIRECTORY_SEPARATOR . $record['ID'] . '.' . $key . '.' . $dest_ext;
+            if (file_exists($src_path)) {
+                copy($src_path, $dest_path);
+            }
+            else {
+                print "Source file $value does not exist\n";
             }
         }
     }
